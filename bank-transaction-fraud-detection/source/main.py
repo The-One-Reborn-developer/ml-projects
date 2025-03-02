faadd82b24a74f8f6b2b724b118f4486f6c02a60
The One Reborn <mortonmarrion@gmail.com>
2025-03-02 21:21:20 +0300

    refactor: feature engineer table column and remove high-cardinality columns

--- bank-transaction-fraud-detection/source/main.py
+++ bank-transaction-fraud-detection/source/main.py
@@ -1,3 +1,5 @@
+import pandas
+
 from kagglehub import dataset_download
 
 from pathlib import Path
@@ -14,17 +16,24 @@ from fastai.tabular.all import (
 
 dataset_path_string = dataset_download(
     'marusagar/bank-transaction-fraud-detection')
-dataset_path = Path(dataset_path_string) / 'Bank_Transaction_Fraud_Detection.csv'
+dataset_path = Path(dataset_path_string) / \
+    'Bank_Transaction_Fraud_Detection.csv'
+
+data_frame = pandas.read_csv(dataset_path)
+data_frame['Transaction_Hour'] = pandas.to_datetime(
+    data_frame['Transaction_Time'], format='%H:%M:%S').dt.hour
+data_frame.drop(columns=['Transaction_Time'], inplace=True)
 
 data_block = TabularDataLoaders.from_csv(
     dataset_path,
     y_names='Is_Fraud',
-    cat_names=['Customer_ID', 'Gender', 'State', 'City', 'Bank_Branch',
-               'Account_Type', 'Transaction_ID', 'Transaction_Date', 'Transaction_Time',
-               'Merchant_ID', 'Transaction_Type', 'Merchant_Category',
-               'Transaction_Device', 'Device_Type', 'Customer_Contact', 'Transaction_Location',
-               'Transaction_Currency', 'Transaction_Description', 'Customer_Email'],
-    cont_names=['Age', 'Transaction_Amount', 'Account_Balance'],
+    cat_names=['Gender', 'State', 'City', 'Bank_Branch', 'Account_Type',
+               'Transaction_ID', 'Transaction_Date', 'Transaction_Type',
+               'Merchant_Category', 'Transaction_Device', 'Device_Type',
+               'Customer_Contact', 'Transaction_Location', 'Transaction_Currency',
+               'Transaction_Description', 'Customer_Email'],
+    cont_names=['Age', 'Transaction_Amount',
+                'Account_Balance', 'Transaction_Hour'],
     procs=[Categorify, FillMissing, Normalize]
 )
 

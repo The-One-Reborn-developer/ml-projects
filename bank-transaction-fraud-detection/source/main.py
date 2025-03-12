4aba292214f7cb37dde2415aeb672d0c20ae8ed3
The One Reborn <mortonmarrion@gmail.com>
2025-03-12 10:04:33 +0300

    refactor: rewrite bank fraud project using sklearn

--- bank-transaction-fraud-detection/source/main.py
+++ bank-transaction-fraud-detection/source/main.py
@@ -4,39 +4,32 @@ from kagglehub import dataset_download
 
 from pathlib import Path
 
-from fastai.tabular.all import (
-    TabularDataLoaders,
-    tabular_learner,
-    accuracy,
-    Categorify,
-    FillMissing,
-    Normalize
-)
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import OneHotEncoder
 
 
 dataset_path_string = dataset_download(
     'marusagar/bank-transaction-fraud-detection')
 dataset_path = Path(dataset_path_string) / \
     'Bank_Transaction_Fraud_Detection.csv'
+encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
 
 data_frame = pandas.read_csv(dataset_path)
 data_frame['Transaction_Hour'] = pandas.to_datetime(
     data_frame['Transaction_Time'], format='%H:%M:%S').dt.hour
 data_frame.drop(columns=['Transaction_Time'], inplace=True)
 
-data_block = TabularDataLoaders.from_df(
-    data_frame,
-    y_names='Is_Fraud',
-    cat_names=['Gender', 'State', 'City', 'Bank_Branch', 'Account_Type',
-               'Transaction_ID', 'Transaction_Date', 'Transaction_Type',
-               'Merchant_Category', 'Transaction_Device', 'Device_Type',
-               'Customer_Contact', 'Transaction_Location', 'Transaction_Currency',
-               'Transaction_Description', 'Customer_Email'],
-    cont_names=['Age', 'Transaction_Amount',
-                'Account_Balance', 'Transaction_Hour'],
-    procs=[Categorify, FillMissing, Normalize]
-)
+y = data_frame['Is_Fraud']
+features = ['Gender', 'State', 'City', 'Bank_Branch', 'Account_Type',
+            'Transaction_ID', 'Transaction_Date', 'Transaction_Type',
+            'Merchant_Category', 'Transaction_Device', 'Device_Type',
+            'Customer_Contact', 'Transaction_Location', 'Transaction_Currency',
+            'Transaction_Description', 'Customer_Email', 'Age', 'Transaction_Amount',
+            'Account_Balance', 'Transaction_Hour']
 
+data_frame_for_encoding = data_frame[features]
 
-learner = tabular_learner(data_block, metrics=accuracy)
-learner.fit_one_cycle(3)
+final_data_frame = encoder.fit_transform(data_frame_for_encoding)
+
+training_model = RandomForestRegressor(random_state=1)
+training_model.fit(final_data_frame, y)

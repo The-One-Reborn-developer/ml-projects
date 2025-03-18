e99edac0beafa7a5c2f24d166605c94991505172
The One Reborn <mortonmarrion@gmail.com>
2025-03-18 22:03:16 +0300

    feat: replace pipeline with tokenizer and model

--- nlp-course/main.py
+++ nlp-course/main.py
@@ -1,10 +1,23 @@
-from transformers import pipeline
+import torch
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
 
-ner = pipeline('question-answering')
-print(
-    ner(
-        question='What is my job?',
-        context='My name is Adil and I work remotely at Anvio as a Software Engineer'
-    )
-)
+model = 'distilbert-base-uncased-finetuned-sst-2-english'
+tokenizer = AutoTokenizer.from_pretrained(model)
+model = AutoModelForSequenceClassification.from_pretrained(model)
+
+inputs = [
+    "This guy tweaking",
+    "I'll have two number nines, a number nine large, a number six with extra dip, two number fourty fives, one with cheese, and a large soda.",
+    "Dat's good"
+]
+tokenized_inputs = tokenizer(inputs, truncation=True, padding=True, return_tensors='pt')
+
+outputs = model(**tokenized_inputs)
+
+predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+print(predictions)
+
+classification = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+print(classification(inputs))

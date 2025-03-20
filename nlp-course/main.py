bad6d3c82f62da773cb21b7b90f799c1d584a414
The One Reborn <mortonmarrion@gmail.com>
2025-03-20 10:21:51 +0300

    feat: manual tokenization and attention mask forming

--- nlp-course/main.py
+++ nlp-course/main.py
@@ -1,13 +1,25 @@
 import torch
 
-from transformers import AutoTokenizer, BertModel
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 
-MODEL = BertModel.from_pretrained('bert-base-cased')
-TOKENIZER = AutoTokenizer.from_pretrained('bert-base-cased')
+CHECKPOINT = "distilbert-base-uncased-finetuned-sst-2-english"
+TOKENIZER = AutoTokenizer.from_pretrained(CHECKPOINT)
+MODEL = AutoModelForSequenceClassification.from_pretrained(CHECKPOINT)
 
-input_ids = TOKENIZER(['Hello!', 'Cool.', 'Nice!'])['input_ids']
-tensor = torch.tensor(input_ids)
 
-output = MODEL(tensor)
-print(output)
+first_sequence = "I've been waiting for a HuggingFace course my whole life."
+second_sequence = "I hate this so much!"
+first_ids = TOKENIZER.convert_tokens_to_ids(TOKENIZER.tokenize(first_sequence))
+second_ids = TOKENIZER.convert_tokens_to_ids(TOKENIZER.tokenize(second_sequence))
+for i in range(max(len(first_ids), len(second_ids)) - min(len(first_ids), len(second_ids))):
+    second_ids.append(TOKENIZER.pad_token_id)
+first_attention_mask = [1 if id != 0 else 0 for id in first_ids]
+second_attention_mask = [1 if id != 0 else 0 for id in second_ids]
+batched_ids = []
+batched_ids.append(first_ids)
+batched_ids.append(second_ids)
+batched_attention_masks = []
+batched_attention_masks.append(first_attention_mask)
+batched_attention_masks.append(second_attention_mask)
+output = MODEL(torch.tensor(batched_ids), attention_mask=torch.tensor(batched_attention_masks))

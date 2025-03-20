0b52dbc5787e03f8b3feeb348e8a2fe6aa2ee389
The One Reborn <mortonmarrion@gmail.com>
2025-03-20 10:36:59 +0300

    feat: putting it all together

--- nlp-course/main.py
+++ nlp-course/main.py
@@ -1,5 +1,4 @@
 import torch
-
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 
@@ -8,18 +7,10 @@ TOKENIZER = AutoTokenizer.from_pretrained(CHECKPOINT)
 MODEL = AutoModelForSequenceClassification.from_pretrained(CHECKPOINT)
 
 
-first_sequence = "I've been waiting for a HuggingFace course my whole life."
-second_sequence = "I hate this so much!"
-first_ids = TOKENIZER.convert_tokens_to_ids(TOKENIZER.tokenize(first_sequence))
-second_ids = TOKENIZER.convert_tokens_to_ids(TOKENIZER.tokenize(second_sequence))
-for i in range(max(len(first_ids), len(second_ids)) - min(len(first_ids), len(second_ids))):
-    second_ids.append(TOKENIZER.pad_token_id)
-first_attention_mask = [1 if id != 0 else 0 for id in first_ids]
-second_attention_mask = [1 if id != 0 else 0 for id in second_ids]
-batched_ids = []
-batched_ids.append(first_ids)
-batched_ids.append(second_ids)
-batched_attention_masks = []
-batched_attention_masks.append(first_attention_mask)
-batched_attention_masks.append(second_attention_mask)
-output = MODEL(torch.tensor(batched_ids), attention_mask=torch.tensor(batched_attention_masks))
+sequences = ["I've been waiting for a HuggingFace course my whole life.", "So have I!"]
+tokens = TOKENIZER(sequences, padding=True, truncation=True, return_tensors='pt')
+output = MODEL(**tokens)
+
+probabilities = torch.softmax(output.logits, dim=1)
+predictions = torch.argmax(probabilities, dim=1)
+print(predictions)

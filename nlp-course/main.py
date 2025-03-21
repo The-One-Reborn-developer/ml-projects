6a760051c983d51f18298e413b8e1731f0faf0f4
The One Reborn <mortonmarrion@gmail.com>
2025-03-21 20:40:16 +0300

    feat: use datasets library

--- nlp-course/main.py
+++ nlp-course/main.py
@@ -1,16 +1,6 @@
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from datasets import load_dataset
 
 
-CHECKPOINT = "distilbert-base-uncased-finetuned-sst-2-english"
-TOKENIZER = AutoTokenizer.from_pretrained(CHECKPOINT)
-MODEL = AutoModelForSequenceClassification.from_pretrained(CHECKPOINT)
-
-
-sequences = ["I've been waiting for a HuggingFace course my whole life.", "So have I!"]
-tokens = TOKENIZER(sequences, padding=True, truncation=True, return_tensors='pt')
-output = MODEL(**tokens)
-
-probabilities = torch.softmax(output.logits, dim=1)
-predictions = torch.argmax(probabilities, dim=1)
-print(predictions)
+raw_dataset = load_dataset("glue", "mrpc")
+train_raw_dataset = raw_dataset['train']
+print(train_raw_dataset.features)

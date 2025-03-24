30d911b9a3e91ee30cdd745bca78e935ec30f8d1
The One Reborn <mortonmarrion@gmail.com>
2025-03-24 10:51:21 +0300

    feat: batch collator dataset preprocessing

--- nlp-course/main.py
+++ nlp-course/main.py
@@ -1,6 +1,20 @@
 from datasets import load_dataset
+from transformers import AutoTokenizer, DataCollatorWithPadding
+from pprint import pprint
 
 
+def tokenize_function(example):
+    return tokenizer(example['sentence1'], example['sentence2'], truncation=True)
+
+
+checkpoint = 'bert-base-uncased'
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+data_collator = DataCollatorWithPadding(tokenizer)
 raw_dataset = load_dataset("glue", "mrpc")
-train_raw_dataset = raw_dataset['train']
-print(train_raw_dataset.features)
+
+tokenized_dataset = raw_dataset.map(tokenize_function, batched=True)
+samples = {key: value for key, value in tokenized_dataset['train'][:8].items() if key not in {'sentence1', 'sentence2', 'idx'}}
+print([len(sample) for sample in samples['input_ids']])
+
+batch = data_collator(samples)
+print({key: value.shape for key, value in batch.items()})

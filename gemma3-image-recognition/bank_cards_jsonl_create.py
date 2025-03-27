59f237dbc6b799e4b68f42d757392bf32ebeb9c6
The One Reborn <mortonmarrion@gmail.com>
2025-03-27 16:05:55 +0300

    refactor: correct jsonl format for model input

--- gemma3-image-recognition/bank_cards_jsonl_create.py
+++ gemma3-image-recognition/bank_cards_jsonl_create.py
@@ -1,8 +1,14 @@
 import os
 import json
 import argparse
+import base64
 
 from prompts import SYSTEM_PROMPT
+
+
+def base64_image_encode(image_path: str) -> str:
+    with open(image_path, mode='rb') as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
 
 
 def create_jsonl(image_urls_file, data_dir, output_file):
@@ -10,32 +16,29 @@ def create_jsonl(image_urls_file, data_dir, output_file):
         image_urls = [line.strip() for line in f]
 
     with open(output_file, 'w') as outfile:
-        for i, img_url in enumerate(image_urls, start=1):
+        for i, _ in enumerate(image_urls, start=1):
             img_json_file = os.path.join(data_dir, f'{i:d}.json')
 
             if os.path.exists(img_json_file):
                 with open(img_json_file, 'r') as json_file:
                     img_json = json.load(json_file)
+                    formatted_json = f"```json\n{json.dumps(img_json, indent=4, ensure_ascii=False)}\n```"
             else:
-                print(f"Файл {img_json_file} не найден, пропускаем.")
+                print(f"File {img_json_file} not found, skipping.")
                 continue
 
-            prompts_record = [
-                    {
-                        "role": "system",
-                        "content": SYSTEM_PROMPT
-                    },
-                    {
-                        "role": "user",
-                        "images": os.path.join(data_dir, f'{i}.jpg')
-                    },
-                    {
-                        "role": "assistant",
-                        "content": json.dumps(img_json, ensure_ascii=False)
-                    }
-                ]
+            image_path = os.path.join(data_dir, f'{i}.jpg')
+            image = base64_image_encode(image_path)
 
-            outfile.write(json.dumps(prompts_record, ensure_ascii=False) + '\n')
+            record = {
+                "messages": [
+                    {"role": "system", "content": SYSTEM_PROMPT},
+                    {"role": "user", "images": [image]},
+                    {"role": "assistant", "content": formatted_json}
+                ]
+            }
+
+            outfile.write(json.dumps(record, ensure_ascii=False) + '\n')
 
 
 if __name__ == "__main__":

ac08b5da68044d2547cadfcde8604697a6b8450b
The One Reborn <mortonmarrion@gmail.com>
2025-03-30 18:30:48 +0300

    feat: implement collation on sst2 dataset

--- nlp-course/main.py
+++ nlp-course/main.py
@@ -4,17 +4,16 @@ from pprint import pprint
 
 
 def tokenize_function(example):
-    return tokenizer(example['sentence1'], example['sentence2'], truncation=True)
+    return tokenizer(example['sentence'], truncation=True)
 
 
 checkpoint = 'bert-base-uncased'
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 data_collator = DataCollatorWithPadding(tokenizer)
-raw_dataset = load_dataset("glue", "mrpc")
+raw_dataset = load_dataset("glue", "sst2")
 
 tokenized_dataset = raw_dataset.map(tokenize_function, batched=True)
-samples = {key: value for key, value in tokenized_dataset['train'][:8].items() if key not in {'sentence1', 'sentence2', 'idx'}}
-print([len(sample) for sample in samples['input_ids']])
+samples = {key: value for key, value in tokenized_dataset['train'][:8].items() if key not in {'sentence', 'idx'}}
 
 batch = data_collator(samples)
-print({key: value.shape for key, value in batch.items()})
+print({key: value for key, value in batch.items()})

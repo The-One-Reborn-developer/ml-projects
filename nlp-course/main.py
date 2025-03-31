718f374cfb64f1450295c0a7f04e6dbf55a91978
The One Reborn <mortonmarrion@gmail.com>
2025-03-31 22:19:40 +0300

    feat: setup fine tuning

--- nlp-course/main.py
+++ nlp-course/main.py
@@ -1,19 +1,16 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer, DataCollatorWithPadding
+from transformers import AutoTokenizer, DataCollatorWithPadding, TrainingArguments
 from pprint import pprint
 
 
 def tokenize_function(example):
-    return tokenizer(example['sentence'], truncation=True)
+    return tokenizer(example['sentence1'], example['sentence2'], truncation=True)
 
 
 checkpoint = 'bert-base-uncased'
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 data_collator = DataCollatorWithPadding(tokenizer)
-raw_dataset = load_dataset("glue", "sst2")
-
+raw_dataset = load_dataset("glue", "mrpc")
 tokenized_dataset = raw_dataset.map(tokenize_function, batched=True)
-samples = {key: value for key, value in tokenized_dataset['train'][:8].items() if key not in {'sentence', 'idx'}}
 
-batch = data_collator(samples)
-print({key: value for key, value in batch.items()})
+training_arguments = TrainingArguments()

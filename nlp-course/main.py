64b8d2d2e0ae2b9655fe7e0765b77f64802b9484
The One Reborn <mortonmarrion@gmail.com>
2025-04-03 21:49:21 +0300

    feat: model training

--- nlp-course/main.py
+++ nlp-course/main.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer, DataCollatorWithPadding, TrainingArguments
+from transformers import AutoTokenizer, DataCollatorWithPadding, TrainingArguments, AutoModelForSequenceClassification, Trainer
 from pprint import pprint
 
 
@@ -14,3 +14,15 @@ raw_dataset = load_dataset("glue", "mrpc")
 tokenized_dataset = raw_dataset.map(tokenize_function, batched=True)
 
 training_arguments = TrainingArguments()
+
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+
+trainer = Trainer(
+    model,
+    training_arguments,
+    train_dataset=tokenized_dataset['train'],
+    eval_dataset=tokenized_dataset['validation'],
+    data_collator=data_collator,
+    tokenizer=tokenizer
+)
+trainer.train()
